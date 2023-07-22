fb2b0eba99b297fb4d350432707bbf2238eef3e7
IcePuff44 <72537714+IcePuff44@users.noreply.github.com>
2023-07-22 20:17:45 +0800

    Update BarChart.js

--- components/BarChart.js
+++ components/BarChart.js
@@ -7,23 +7,23 @@ const BarChart = () => {
   return (
     <div className="bg-white py-2 px-2 mx-8 my-1 rounded-xl shadow-md">
       <div className="flex flex-row justify-around pb-4">
-        <p className="text-xl font-bold px-1 py-4">Carbon Footprint</p>
+        <p className="text-xl font-bold px-1 py-4">Reviews</p>
         <div className="flex flex-row gap-3 justify-center items-center">
           <div className="flex flex-row gap-1 items-center">
             <div className="rounded-full bg-[#57CC78] w-[9px] h-[9px] "></div>
-            <p className="text-xs font-semibold">Spinning</p>
+            <p className="text-xs font-semibold">A</p>
           </div>
           <div className="flex flex-row gap-1 items-center">
             <div className="rounded-full bg-[#55DBDB] w-[9px] h-[9px] "></div>
-            <p className="text-xs font-semibold">Transportation</p>
+            <p className="text-xs font-semibold">B</p>
           </div>
           <div className="flex flex-row gap-1 items-center">
             <div className="rounded-full bg-[#E2FF32] w-[9px] h-[9px] "></div>
-            <p className="text-xs font-semibold">Carding</p>
+            <p className="text-xs font-semibold">C</p>
           </div>
           <div className="flex flex-row gap-1 items-center">
             <div className="rounded-full bg-[#FEC102] w-[9px] h-[9px] "></div>
-            <p className="text-xs font-semibold">Heating & Cooling</p>
+            <p className="text-xs font-semibold">D</p>
           </div>
           <Image src={bar} alt="" />
         </div>
