c7c4f4fbcef7d101838bb0d08452efd4d467df15
IcePuff44 <72537714+IcePuff44@users.noreply.github.com>
2023-07-22 20:29:11 +0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,8 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "Greenstitch",
-  description: "Greenstitch Dashboard",
+  title: "HelloWorld",
+  description: "HelloWorld Dashboard",
 };
 
 export default function RootLayout({ children }) {
